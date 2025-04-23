c1b6397e8691b1d2110a8a7c028e2644dfc54777
BondarenkoMM <masya.klas3000@gmail.com>
2025-04-23 23:03:10 +0200

    commit

---
+++ src/main/java/com/bot/vladyslav_bond_back/VladyslavBondBackApplication.java
@@ -0,0 +1,13 @@
+package com.bot.vladyslav_bond_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VladyslavBondBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VladyslavBondBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/vladyslav_bond_back/VladyslavBondBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.vladyslav_bond_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VladyslavBondBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
